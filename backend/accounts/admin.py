4d1db892d884be5904605cff7eedc34549346b12
Carlos-UCH <carlosbrasil254@gmail.com>
2025-11-29 19:30:24 -0300

    feat: admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'organization')
+    list_filter = ('organization', 'role')
+    search_fields = ('user__email', 'user__first_name')
