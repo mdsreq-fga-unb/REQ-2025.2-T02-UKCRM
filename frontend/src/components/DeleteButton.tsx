4e83b1b51cff14488b687274f05fd0aaf307e921
yogi <yogiqhz@gmail.com>
2025-12-01 04:17:01 -0300

    fix: texto com cor errada no botão de excluir funil

--- frontend/src/components/DeleteButton.tsx
+++ frontend/src/components/DeleteButton.tsx
@@ -7,7 +7,12 @@ type DeleteButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 export default function DeleteButton({ label, ...props }: DeleteButtonProps) {
   return (
-    <Button variant="destructive" onClick={props.onClick} disabled={props.disabled}>
+    <Button
+      variant="destructive"
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className="text-white"
+    >
       <XIcon /> {label}
     </Button>
   );
