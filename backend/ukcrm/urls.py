a7d5c66f589dbeb946cbc4376d91d544e841ac13
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-12-02 01:36:51 -0300

    hotfix: última atualização

--- backend/ukcrm/urls.py
+++ backend/ukcrm/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -19,7 +20,6 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
+    # Serve media files in production (required in Docker since nginx proxies to Django)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve media files (required in Docker since nginx proxies to Django)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
